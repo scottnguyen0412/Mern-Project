42cd77ef07b7c5d4ba121984cb65be0d099e7ab8
scottnguyen0412 <scottnguyen0412@gmail.com>
2023-07-11 16:37:16 +0700

    Connect with mongoDb cloud

--- Social-media/README.md
+++ Social-media/README.md
@@ -5,11 +5,12 @@
     <li>npm i express (nodejs)</li>
     <li>npm i mongoose (nodejs with mongoDb)</li>
     <li>npm i nodemon (nodejs)</li>
+    <li>npm install dotenv (nodejs)</li>
 </ul>
 
 <h3>Package used for ReactJs </h3>
 <ul>
-<li>npm install @mui/material @emotion/react @emotion/styled (Material Icons U<li>
+<li>npm install @mui/material @emotion/react @emotion/styled (Material Icons UI<li>
 <li>npm i axios</li>
 <li>npm i moment</li>
 <li>npm i react-router-dom</li>
@@ -20,3 +21,5 @@
 <li>npm i jwt-decode</li>
 <li>npm install react-google-login --legacy-peer-deps</li>
 </ul>
+
+<h3>MongoDb cloud: <link href="https://cloud.mongodb.com/v2#/org/64ad174b5a555f386d7195e5/projects">https://cloud.mongodb.com/v2#/org/64ad174b5a555f386d7195e5/projects</link></h3>
--- Social-media/server/index.js
+++ Social-media/server/index.js
@@ -2,3 +2,29 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from 'cors';
+import 'dotenv/config'
+
+
+const app = express();
+
+// Dòng đầu tiên: sử dụng method json() của body-parser để phân tích dữ liệu dạng JSON.
+// Cụ thể, limit cho phép giới hạn kích thước tối đa của request body là 30MB và extended cho phép phân tích cấu trúc dữ liệu phức tạp hơn.
+app.use(bodyParser.json({limit: "30mb", extended: true}))
+
+// Sử dụng method urlencoded() để phân tích dữ liệu từ request body dạng application/x-www-form-urlencoded
+// sử dụng để giới hạn kích thước dữ liệu và mở rộng cấu trúc dữ liệu của request body.
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+// ----------- Nhờ hai dòng trên ứng dụng có thể phân tích và xử lý các request body có dạng JSON 
+// -----------hoặc application/x-www-form-urlencoded được gửi từ client đến server
+
+app.use(cors())
+
+// Database mongDb connection
+const CONNECTION_URL = process.env.MONGODB_CLOUD
+const PORT = process.env.PORT || 3002;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, console.log(`Server running on Port ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// Tắt tính năng cảnh báo sử dụng findOneAndUpdate() và findOneAndDelete() mặc định của Mongoose
