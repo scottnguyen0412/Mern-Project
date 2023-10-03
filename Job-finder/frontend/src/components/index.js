ec378e4e7a37c54ffb81f422802d235bb1582c5e
scottnguyen0412 <scottnguyen0412@gmail.com>
2023-10-03 18:25:03 +0700

    Add footer UI

--- Job-finder/frontend/src/components/index.js
+++ Job-finder/frontend/src/components/index.js
@@ -1,5 +1,6 @@
 import Navbar from "./navbar/Navbar";
 import Footer from "./footer/Footer";
 import CustomButton from "./CustomButton";
+import TextInput from "./TextInput";
 
-export {Navbar, Footer, CustomButton};
+export {Navbar, Footer, CustomButton, TextInput};
