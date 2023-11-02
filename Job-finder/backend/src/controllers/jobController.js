89a0b039ae0805e2e9f060d832d08173781f2bbc
scottnguyen0412 <scottnguyen0412@gmail.com>
2023-11-02 07:04:07 +0700

    Get all job on BE

--- Job-finder/backend/src/controllers/jobController.js
+++ Job-finder/backend/src/controllers/jobController.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import Jobs from "../models/jobModel";
 
+// Create Job Post
 export const createJob = async (req, res, next) => {
     try {
         const {jobTitle, JobType, 
@@ -40,6 +41,7 @@ export const createJob = async (req, res, next) => {
     }
 }   
 
+// Update Job Post 
 export const updateJob = async (req, res, next) => {
     const { id } = req.params;
     const jobPost = req.body
@@ -56,6 +58,92 @@ export const updateJob = async (req, res, next) => {
             message: "Post Updated Successfully",
             updateJob
         });
+    } catch (error) {
+        console.error(error);
+        res.status(404).json({
+            message: error.message
+        })
+    }
+}
+
+// Get Job Posts
+export const getJobPosts = async (req, res, next) => {
+    try {
+        const {search, sort, location, jtype, exp} = req.query;
+        const types = jtype.split(','); //ex: full-time,part-time
+        const experience = exp.split('-'); //ex: 2-6
+
+        let queryObject = {};
+        if(location){
+            queryObject.location = {$regex: location, $options: "i"};
+        }
+        
+        if(jtype){
+            queryObject.location = {$in: types};
+        }
+
+        if(exp){
+            queryObject.experience = {
+                // $gte (lớn hơn hoặc bằng)
+                $gte: experience[0],
+                // $lte (nhỏ hơn hoặc bằng)
+                $lte: experience[1],
+            }
+        }
+
+        if(search){
+            // search by job title, job type or tags
+            const searchQuery = {
+                $or: [
+                    {jobTitle: {$regex: search, $options: "i"}},
+                    {JobType: {$regex: search, $options: "i"}},
+                ]
+            };
+            queryObject = {...queryObject, ...searchQuery}
+        }
+
+        let queryResult = Jobs.find(queryObject).populate({
+            path: "company",
+            select: "-password"
+        })
+
+        // Sorting
+        switch (sort) {
+            case "Newest":
+                queryResult = queryResult.sort("-createdAt");
+                break;
+            case "Oldest":
+                queryResult = queryResult.sort("createdAt");
+                break;
+            case "A-Z":
+                queryResult = queryResult.sort("jobTitle");
+                break;
+            case "Z-A":
+                queryResult = queryResult.sort("-jobTitle");
+                break;
+            default:
+                // Xử lý khi giá trị sort không khớp với các case trên
+                break;
+        }
+
+        // Paginations
+        const page = Number(req.query.page) || 1
+        
+        // giới hạn bao nhiêu item trong 1 page 
+        const limit = Number(req.query.limit) || 20
+        const skip = (page - 1) * limit;
+
+        const totalJobs = await Jobs.countDocuments(queryResult);
+        const numOfPage = Math.ceil(total/ limit);
+        queryResult = queryResult.limit(limit * page);
+        const jobs = await queryResult;
+        res.status(200).json({
+            success: true,
+            totalJobs,
+            data: jobs,
+            page,
+            numOfPage
+        })
     } catch (error) {
         console.error(error);
         res.status(404).json({
